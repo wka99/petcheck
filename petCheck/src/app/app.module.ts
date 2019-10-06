676832afd032acab23b3690924399e529b80ef88
una97 <yoona607@dongguk.edu>
2019-10-06 15:01:02 +0900

    환경설정

---
+++ petCheck/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+//
+import {firebaseConfig} from './firebase';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ petCheck/src/app/firebase.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCvLf-E_zukl84_wx_uPPY5Z615mBuuoM",
+        authDomain: "petcheck-6a816.firebaseapp.com",
+        databaseURL: "https://petcheck-6a816.firebaseio.com",
+        projectId: "petcheck-6a816",
+        storageBucket: "",
+        messagingSenderId: "565850238910",
+        appId: "1:565850238910:web:0b4cc4fb5aa34ea1fb13ef",
+        measurementId: "G-LHYP1G12V2"
+    
+};

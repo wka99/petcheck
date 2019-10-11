c3245540cb52136fc8c90b008b997b469d01c006
una97 <yoona607@dongguk.edu>
2019-10-12 05:56:18 +0900

    진단서 업로드

--- petCheck2/src/app/medical-chart/medical-chart.page.ts
+++ petCheck2/src/app/medical-chart/medical-chart.page.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {DataFinderService} from '../../data-finder.service';
+import { DataFinderService } from '../../data-finder.service';
+import {AlertController} from '@ionic/angular';
 @Component({
   selector: 'app-medical-chart',
   templateUrl: './medical-chart.page.html',
   styleUrls: ['./medical-chart.page.scss'],
 })
 export class MedicalChartPage implements OnInit {
-array=[];
-price;
+  array = [];
+  price;
+  newValue=0;
   constructor(
-    public dataFinder:DataFinderService
-  ) { 
+    public dataFinder: DataFinderService, 
+    public alertCtrl: AlertController,
+  ) {
     //json데이터 가져오기
-    this.dataFinder.getJSONDataAsync("../../assets/data/csvjson.json").then(data=>{
+    this.dataFinder.getJSONDataAsync("../../assets/data/csvjson.json").then(data => {
       this.SetQueryOptionsData(data);
     });
-   }
-   SetQueryOptionsData(items:any){ //items는 json데이터들
-    this.array= items;//data는 json파일의 id
-    this.price=this.array[0].가격2;
   }
-
+  SetQueryOptionsData(items: any) { //items는 json데이터들
+    this.array = items;//data는 json파일의 id
+    this.price = this.array[0].가격2;
+  }
+  download() {
+    this.alertCtrl.create({
+      header: '',
+      message: '최근 진단서를 업로드 하시겠습니까?',
+      buttons: [{
+        text: '아니요',
+        role: 'cancel'
+      },{
+        text: '네',
+        handler:()=>{
+          this.newValue=1;
+          console.log(this.newValue);
+        }
+      }]
+    }).then(alertEI => {
+      alertEI.present();
+    });
+  }
   ngOnInit() {
   }
 
